32606d4e4b8d4cc35ddd8aef29115cc35660ac2b
SamPerCaparros <samu1667@easv365.dk>
2018-11-19 11:37:44 +0100

    Cors Modified

--- CompanyName.PetShop.RestApi/Startup.cs
+++ CompanyName.PetShop.RestApi/Startup.cs
@@ -59,7 +59,18 @@ namespace CompanyName.PetShop.RestApi
                 };
             });
 
-            services.AddCors();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader()
+                        .AllowAnyMethod());
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.WithOrigins("https://angularonfirebasepetshop.firebaseapp.com").AllowAnyHeader()
+                        .AllowAnyMethod());
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.WithOrigins("https://localhost:4200").AllowAnyHeader()
+                      .AllowAnyMethod());
+            });
 
             if (_env.IsDevelopment())
             {
